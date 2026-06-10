6a785a61e1295b359b9a78f7ed84803f3d60fad3
Kaiquii <112667885+Kaiquii@users.noreply.github.com>
2026-06-10 13:10:03 -0300

    - Corrigindo erros de dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,7 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation(libs.okhttp)
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("androidx.biometric:biometric:1.2.0-alpha05")
     implementation("io.coil-kt:coil-compose:2.7.0")
